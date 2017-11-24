5b6b315312e695454c202acce5d2cd9f069dc416
Jitendra <jitendra.chaudhary@staples.com>
2017-11-24 20:49:03 +0530

    Added the mongo and json parsing examples

--- hello/build.sbt
+++ hello/build.sbt
@@ -1,14 +1,22 @@
 import Dependencies._
+import sbt.Keys.libraryDependencies
 
 lazy val root = (project in file(".")).
   settings(
     inThisBuild(List(
       organization := "com.example",
-      scalaVersion := "2.11.11",
+      scalaVersion := "2.12.4",
       version      := "0.1.0-SNAPSHOT"
     )),
     name := "Hello",
     libraryDependencies += scalaTest % Test
   )
 // https://mvnrepository.com/artifact/org.json4s/json4s-jackson
-libraryDependencies += "org.json4s" % "json4s-jackson_2.10" % "3.1.0"
+libraryDependencies += "org.json4s" % "json4s-jackson_2.12" % "3.6.0-M1"
+
+// https://mvnrepository.com/artifact/org.json4s/json4s-native
+libraryDependencies += "org.json4s" % "json4s-native_2.12" % "3.6.0-M1"
+
+// https://mvnrepository.com/artifact/org.specs2/specs2
+libraryDependencies += "org.specs2" % "specs2_2.10" % "1.12.3" % "test"
+libraryDependencies += "org.mongodb" %% "casbah" % "3.1.1"
--- hello/src/main/scala/example/ReadFileOps.scala
+++ hello/src/main/scala/example/ReadFileOps.scala
@@ -11,22 +11,26 @@ object ReadFileOps extends App{
   def readFile(): Unit ={
 
     println("Love me Do")
-    val source = scala.io.Source.fromFile("milkyway.json")
+    val source = scala.io.Source.fromFile("C:\\Users\\465068\\Repo\\scalapractise\\hello\\milkyway.json")
     val lines =  source.getLines()
 
     for( line <- lines){
-     // println(line.mkString)
-      println(jsonStrToMap(line.mkString))
+      println(line.mkString)
+      var v:JValue = parse(line.mkString) \\ "word"
+      println(v.values)
      // println(mp)
       //mps += mp
     }
     source.close()
   }
-
-  def jsonStrToMap(jsonStr: String): Map[String, Any] = {
-    val json = "{"+jsonStr.substring(43);
-
-    println(json)
-    parse(json).extract[Map[String, String]]
-  }
 }
+/*{"_id":{"$oid":"5949e12a4cb200478209a1fb"},
+"word":"would like to see",
+"type":"phrase",
+"nature":"action",
+  "structure":{
+    "sequence":"noun1-would like to see-noun2-as-sentence2",
+    "who like to see":"noun1",
+    "what like to see":"noun2",
+    "why like to see":"sentence2"}}
+*/
